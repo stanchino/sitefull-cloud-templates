4f483cac0e7f9a39d99d61e89a5c1b9d5e600da3
Stanimir Dimitrov <stanchino@gmail.com>
2016-01-30 19:11:46 +0200

    Fixing CC issues

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,6 @@ preload_app true
 
 before_fork do |_server, _worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
   end
 
@@ -13,10 +12,6 @@ before_fork do |_server, _worker|
 end
 
 after_fork do |_server, _worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
   defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.establish_connection
 end
