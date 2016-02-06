846e8114fc9e9bebfdcb8be69ddcc23b5d5fa985
Stanimir Dimitrov <stanchino@gmail.com>
2016-02-06 09:28:19 +0200

    Fixing CodeClimate issues

--- spec/routing/deployments_routing_spec.rb
+++ spec/routing/deployments_routing_spec.rb
@@ -9,23 +9,27 @@ RSpec.describe DeploymentsController, type: :routing do
     end
 
     it 'routes to #index' do
-      expect(get: "/templates/#{template.id}/deployments").to route_to('deployments#index', template_id: template.id.to_s)
+      expect(get: "/templates/#{template.id}/deployments").to route_to_action 'index'
     end
 
     it 'routes to #new' do
-      expect(get: "/templates/#{template.id}/deployments/new").to route_to('deployments#new', template_id: template.id.to_s)
+      expect(get: "/templates/#{template.id}/deployments/new").to route_to_action 'new'
+    end
+
+    it 'routes to #create' do
+      expect(post: "/templates/#{template.id}/deployments").to route_to_action 'create'
     end
 
     it 'routes to #show' do
       expect(get: '/deployments/1').to route_to('deployments#show', id: '1')
     end
 
-    it 'routes to #create' do
-      expect(post: "/templates/#{template.id}/deployments").to route_to('deployments#create', template_id: template.id.to_s)
-    end
-
     it 'routes to #destroy' do
       expect(delete: "/templates/#{template.id}/deployments/1").to route_to('deployments#destroy', template_id: template.id.to_s, id: '1')
+    end
+
+    def route_to_action(action)
+      route_to("deployments##{action}", template_id: template.id.to_s)
     end
   end
 end
